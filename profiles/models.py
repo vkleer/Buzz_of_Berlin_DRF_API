f8e38892b87296a863b2ada7807e6837043b4ed0
Vilayat Kleer <vkleer@hotmail.com>
2022-10-16 12:17:40 +0000

    Add Sports and Music class to Profile model as TextChoices

--- profiles/models.py
+++ profiles/models.py
@@ -13,16 +13,25 @@ class Profile(models.Model):
     username = models.CharField(max_length=75, blank=True)
     name = models.CharField(max_length=75, blank=True)
     district = models.CharField(
-        max_length=2,
+        max_length=50,
         choices=Districts.choices,
         default=Districts.MITTE,
     )
     languages = models.CharField(
-        max_length=2,
+        max_length=20,
         choices=Languages.choices,
         default=Languages.ENGLISH,
     )
-    interests = models.CharField(max_length=255, blank=True)
+    music = models.CharField(
+        max_length=20,
+        choices=Music.choices,
+        default=Music.POP,
+    )
+    sports = models.CharField(
+        max_length=20,
+        choices=Music.choices,
+        default=Music.POP,
+    )
     description = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_profile_image-01_iotmha'
@@ -77,6 +86,87 @@ class Profile(models.Model):
         STEGLITZZEHLENDORF = 'Steglitz-Zehlendorf'
         TEMPELHOFSCHONEBERG = 'Tempelhof-Schöneberg'
         TREPTOWKOPENICK = 'Treptow-Köpenick'
+
+    class Music(models.TextChoices):
+        SIXTIES = '60s'
+        SEVENTIES = '70s'
+        EIGHTIES = '80s'
+        NINETIES = '90s'
+        POP = 'Pop'
+        SYNTHPOP = 'Synthpop'
+        DISCO = 'Disco'
+        NEWWAVE = 'New Wave'
+        DARKWAVE = 'Dark Wave'
+        INDUSTRIAL = 'Industrial'
+        PUNK = 'Punk'
+        ROCK = 'Rock'
+        INDIE = 'Indie'
+        EMO = 'Emo'
+        GRUNGE = 'Grunge'
+        METAL = 'Metal'
+        BLUES = 'Blues'
+        JAZZ = 'Jazz'
+        CLASSICAL = 'Classical'
+        COUNTRY = 'Country'
+        FOLK = 'Folk'
+        HIPHOP = 'Hip-hop'
+        TRIPHOP = 'Trip-hop'
+        TRAP = 'Trap'
+        REGGAE = 'Reggae'
+        SOUL = 'Soul'
+        RNB = 'R&B'
+        TECHNO = 'Techno'
+        HOUSE = 'House'
+        TRANCE = 'Trance'
+        DRUMANDBASS = 'Drum & Bass'
+        EDM = 'EDM'
+        EBM = 'EBM'
+        KPOP = 'K-Pop'
+        JPOP = 'J-Pop'
+        LATIN = 'Latin'
+
+    class Sports(models.TextChoices):
+        FOOTBALL = 'Football'
+        BASKETBALL = 'Basketball'
+        TENNIS = 'Tennis'
+        VOLLEYBALL = 'Volleyball'
+        HANDBALL = 'Handball'
+        BADMINTON = 'Badminton'
+        SWIMMING = 'Swimming'
+        BOXING = 'Boxing'
+        TABLETENNIS = 'Tabletennis'
+        SKIING = 'Skiing'
+        ICESKATING = 'Ice skating'
+        ROLLERSKATING = 'Roller skating'
+        CRICKET = 'Cricket'
+        RUGBY = 'Rugby'
+        DARTS = 'Darts'
+        BOWLING = 'Bowling'
+        HOCKEY = 'Hockey'
+        ICEHOCKEY = 'Ice hockey'
+        KARATE = 'Karate'
+        HORSERIDING = 'Horse riding'
+        SNOWBOARDING = 'Snowboarding'
+        SKATEBOARDING = 'Skateboarding'
+        CYCLING = 'Cycling'
+        ARCHERY = 'Archery'
+        GYMNASTICS = 'Gymnastics'
+        FIGURESKATING = 'Figureskating'
+        ROCKCLIMBING = 'Rockclimbing'
+        BOULDERING = 'Bouldering'
+        JUDO = 'Judo'
+        KICKBOXING = 'Kickboxing'
+        WRESTLING = 'Wrestling'
+        JUJUTSU = 'Jujutsu'
+        MMA = 'MMA'
+        TAEKWONDO = 'Taekwondo'
+        AIKIDO = 'Aikido'
+        FENCING = 'Fencing'
+        GOLF = 'Golf'
+        BOWLING = 'Bowling'
+        DANCING = 'Dancing'
+        FISHING = 'Fishing'
+        ROWING = 'Rowing'
 
     def __str__(self):
         """
