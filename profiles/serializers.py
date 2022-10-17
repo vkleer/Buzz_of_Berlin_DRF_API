93ddb987f6e5291b236d6fdc7473a1d8e2d8e00e
Vilayat Kleer <vkleer@hotmail.com>
2022-10-17 11:40:56 +0000

    Create ProfileSerializer for Profile model

---
+++ profiles/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Profile
+        fields = [
+            'id', 'owner', 'creation_date', 'updated_date', 'name',
+            'district', 'languages', 'music', 'sports', 'description',
+        ]
