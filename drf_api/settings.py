b6dde4ef6bd9ad6b565287470d6f3cb38453816e
Vilayat Kleer <vkleer@hotmail.com>
2022-10-18 21:05:23 +0000

    Create PostList view

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -24,6 +24,8 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Mapbox API key
+MAPBOX_KEY = os.environ.get('MAPBOX_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostList(APIView):
+    """
+    A class for the PostList API view
+    """
+    def get(self, request):
+        profiles = Post.objects.all()
+        serializer = PostSerializer()(
+            profiles, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
