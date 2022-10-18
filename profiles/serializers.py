79e8c2727cf65f499068ba62347a57691b8a835b
Vilayat Kleer <vkleer@hotmail.com>
2022-10-18 11:43:59 +0000

    Create is_owner field for ProfileSerializer, add request key/value pair to context in profiles views, set is_owner field in ProfileSerializer to the request from the context

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -7,13 +7,18 @@ class ProfileSerializer(serializers.ModelSerializer):
     A class for the ProfileSerializer model serializer
     """
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'creation_date', 'updated_date', 'name',
             'district', 'languages', 'music', 'sports', 'description',
-            'image',
+            'image', 'is_owner',
         ]
 
     languages = fields.MultipleChoiceField(choices=Profile.Languages)
