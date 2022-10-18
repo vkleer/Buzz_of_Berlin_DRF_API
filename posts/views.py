3b3fa187f0f40f8f1338a04b86bf37fb33d7b22d
Vilayat Kleer <vkleer@hotmail.com>
2022-10-18 21:49:56 +0000

    Add delete method to PostDetail view

--- posts/views.py
+++ posts/views.py
@@ -65,4 +65,13 @@ class PostDetail(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
