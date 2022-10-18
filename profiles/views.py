8b712ccccae3bd9d24e6ec587442de2bde7be241
Vilayat Kleer <vkleer@hotmail.com>
2022-10-18 10:32:25 +0000

    Add put method to ProfileDetail view to implement profile updating

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -19,6 +20,8 @@ class ProfileDetail(APIView):
     """
     A class for the ProfileDetail API view
     """
+    serializer_class = ProfileSerializer
+    
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
@@ -30,3 +33,11 @@ class ProfileDetail(APIView):
         profile = self.get_object(pk)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
