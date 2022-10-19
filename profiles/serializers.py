a5c6b3740d1d33576ce61edd707e0c297fd3fec3
Vilayat Kleer <vkleer@hotmail.com>
2022-10-19 15:03:12 +0000

    Create following_id, posts_count, followers_count and following_count fields for ProfileSerializer, create get_following_id method for ProfileSerializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers, fields
 from .models import Profile
+from followers.models import Follower
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -8,6 +9,10 @@ class ProfileSerializer(serializers.ModelSerializer):
     """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         """
@@ -17,6 +22,20 @@ class ProfileSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_following_id(self, obj):
+        """
+        Checks if the logged in user is following any other profiles.
+        Sets the following_id field equal to the corresponding following
+        instance
+        """
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return following.id if following else None
+        return None
+
     class Meta:
         """
         Returns the fields to be displayed from the Profile model
@@ -25,7 +44,8 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'creation_date', 'updated_date', 'name',
             'district', 'languages', 'music', 'sports', 'description',
-            'image', 'is_owner',
+            'image', 'is_owner', 'following_id', 'posts_count',
+            'followers_count', 'following_count',
         ]
 
     languages = fields.MultipleChoiceField(choices=Profile.Languages)
