676ed6608d92cdf1549a7a32140df9ac3aeece3b
Vilayat Kleer <vkleer@hotmail.com>
2022-10-19 14:12:05 +0000

    Create LikeDetail view, add LikeDetail view to likes' urls file

--- likes/views.py
+++ likes/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
 from likes.models import Like
 from likes.serializers import LikeSerializer
 
@@ -10,3 +11,9 @@ class LikeList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
