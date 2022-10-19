4f4254389b8a75c5064529a95b986997f2d32bd6
Vilayat Kleer <vkleer@hotmail.com>
2022-10-19 13:07:37 +0000

    Create CommentList view, create urls file for comments app and add to main urls file

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
 
-# Create your views here.
+
+class CommentList(generics.ListCreateAPIView):
+    """
+    A class for the CommentList API view
+    """
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
