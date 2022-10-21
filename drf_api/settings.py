04222a685e5c65006aece42a0134b8fd57d83d98
Vilayat Kleer <vkleer@hotmail.com>
2022-10-21 20:10:35 +0000

    Create Procfile for Heroku deployment, set allowed hosts in settings file

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -63,8 +63,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
+    'localhost',
+]
 
 # Application definition
 
