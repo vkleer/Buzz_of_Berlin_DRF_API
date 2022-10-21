f652b8214b35cfcfe45c4b7480628d15fd224ec0
Vilayat Kleer <vkleer@hotmail.com>
2022-10-21 18:08:46 +0000

    Create urls file for events app, add events' urls to main urls file

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
     path('', include('recommendations.urls')),
+    path('', include('events.urls')),
     path('', include('comments.urls')),
     path('', include('likes.urls')),
     path('', include('followers.urls')),
