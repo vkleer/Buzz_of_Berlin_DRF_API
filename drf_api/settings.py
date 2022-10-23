75ecaa556b0b7cbfd780fca44aca19cfd9aa8b5c
Vilayat Kleer <vkleer@hotmail.com>
2022-10-23 19:30:32 +0000

    Fix: Import re in settings file

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import re
 import dj_database_url
 from corsheaders.defaults import default_headers, default_methods
 
