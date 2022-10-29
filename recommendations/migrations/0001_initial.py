1a65a398e9c836547e28e188626b8ea4e63d637c
Vilayat Kleer <vkleer@hotmail.com>
2022-10-29 14:44:51 +0000

    Remove location-mapbox import in initial.py

--- recommendations/migrations/0001_initial.py
+++ recommendations/migrations/0001_initial.py
@@ -3,7 +3,6 @@
 from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
-import mapbox_location_field.models
 
 
 class Migration(migrations.Migration):
@@ -23,7 +22,6 @@ class Migration(migrations.Migration):
                 ('updated_date', models.DateTimeField(auto_now=True)),
                 ('title', models.CharField(max_length=255)),
                 ('location_name', models.CharField(max_length=255, unique=True)),
-                ('location_mapbox', mapbox_location_field.models.LocationField(map_attrs={})),
                 ('entry_fee', models.CharField(choices=[('Free', 'Free'), ('€1,00 to €5,00', 'Onetofive'), ('€5,00 to €10,00', 'Fivetoten'), ('€10,00 to €15,00', 'Tentofifteen'), ('€15,00 to €20,00', 'Fifteentotwenty'), ('€20,00 to €30,00', 'Twentytothirty'), ('€30,00 to €50,00', 'Thirtytofifty'), ('€50,00 to €100,00', 'Fiftytohundred'), ('€100,00 or more', 'Overhundred')], max_length=17)),
                 ('price', models.CharField(choices=[('Free', 'Free'), ('€', 'Cheap'), ('€€', 'Average'), ('€€€', 'Aboveaverage'), ('€€€€', 'Expensive')], max_length=12)),
                 ('district', models.CharField(choices=[('Friedrichshain-Kreuzberg', 'Friedrichshain Kreuzberg'), ('Lichtenberg', 'Lichtenberg'), ('Marzahn-Hellersdorf', 'Marzahn Hellersdorf'), ('Mitte', 'Mitte'), ('Neukölln', 'Neukolln'), ('Pankow', 'Pankow'), ('Reinickendorf', 'Reinickendorf'), ('Spandau', 'Spandau'), ('Steglitz-Zehlendorf', 'Steglitz Zehlendorf'), ('Tempelhof-Schöneberg', 'Tempelhof Schoneberg'), ('Treptow-Köpenick', 'Treptow Kopenick')], max_length=50)),
