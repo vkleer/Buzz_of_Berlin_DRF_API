220cd202f04b68559213dcdc39dbaf3ec3431cd0
Vilayat Kleer <vkleer@hotmail.com>
2022-10-30 20:40:51 +0000

    Add recommandations_count and events_count to ProfileDetail queryset

--- profiles/views.py
+++ profiles/views.py
@@ -48,6 +48,8 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
+        recommendations_count=Count('owner__recommendation', distinct=True),
+        events_count=Count('owner__event', distinct=True),
         followers_count=Count('owner__followed', distinct=True),
         following_count=Count('owner__following', distinct=True),
     ).order_by('-creation_date')
