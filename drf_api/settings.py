0e7905507298c7d829982bc09f65b64ccf476804
Vilayat Kleer <vkleer@hotmail.com>
2022-10-31 23:35:37 +0000

    Set static files settings to fix CSS not loading on deployed API

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -201,6 +201,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = ('cloudinary_storage.storage.StaticHashedCloudinaryStorage')
+STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'static')]
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
