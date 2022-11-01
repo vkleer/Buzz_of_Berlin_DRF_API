94bdb47f8b55169a4cdde967e5d60aae593d3065
Vilayat Kleer <vkleer@hotmail.com>
2022-11-01 00:07:40 +0000

    Add defaults file storage to settings file

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -202,8 +202,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
